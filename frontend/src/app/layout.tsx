77017db2c2e26d8154c90d7f7a9826fe600368b6
DouglasC627 <douglas0627@gmail.com>
2025-10-19 00:09:48 +0800

    fix ts rror for globals.css

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -26,9 +26,7 @@ export const metadata: Metadata = {
       { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/favicon/favicon.ico', sizes: 'any' },
     ],
-    apple: [
-      { url: '/favicon/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
-    ],
+    apple: [{ url: '/favicon/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
     other: [
       { rel: 'android-chrome-192x192', url: '/favicon/android-chrome-192x192.png' },
       { rel: 'android-chrome-512x512', url: '/favicon/android-chrome-512x512.png' },
